25053f1f05202e53edca39e3158a577ab1afb3d0
Denis Curdoglo <curdogld@tcd.ie>
2025-02-02 00:43:13 +0000

    Basic set up for backend using Python/Flask and for frontend using Js/React.

---
+++ backend/main.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+# API Route (Test)
+@app.route("/tests")
+def tests():
+    return {"tests": ["Hi I am Test 1", "And I am Test 2", "And don't forget test 3"]}
+
+if __name__ == "__main__":
+    app.run(debug = True)
