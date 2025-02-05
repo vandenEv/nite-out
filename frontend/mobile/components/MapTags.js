45712a0b3da5b4133ac15204c47c30e5b34cc944
vandenEv <vandenev@tcd.ie>
2025-02-05 20:40:45 +0000

    Added tag buttons

---
+++ frontend/mobile/components/MapTags.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+import { View, ScrollView, TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const MapTags = ({ tags = [], onSelectedTag }) => {
+    const [selectedTag, setSelectedTag] = useState(null);
+
+    const handlePressedTag = (tag) => {
+        setSelectedTag(tag);
+        if(onSelectedTag) {
+            onSelectedTag(tag); 
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <ScrollView 
+                horizontal 
+                showsHorizontalScrollIndicator={false} 
+                contentContainerStyle={styles.scrollContainer}>
+                {tags.map((tag, index) => (
+                    <TouchableOpacity
+                        key={index}
+                        style={[
+                            styles.button,
+                            selectedTag === tag ? styles.selectedButton : null,
+                            selectedTag === tag ? styles.selectedText : null,
+                        ]}                        
+                        onPress={() => handlePressedTag(tag)}
+                    >
+                        <Text style={[styles.buttonText, selectedTag === tag && styles.selectedText]}>
+                            {tag}
+                        </Text>
+                    </TouchableOpacity>
+                ))};
+            </ScrollView>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        paddingVertical: 5,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '7%',
+    }, 
+    scrollContainer: {
+        paddingHorizontal: 10,
+    },
+    button: {
+        paddingVertical: 0,
+        paddingHorizontal: 16,
+        borderRadius: 15,
+        backgroundColor: "#FBDBE9",
+        justifyContent: 'center',
+        marginRight: 8,
+    },
+    selectedButton: {
+        backgroundColor: "#FF006E",
+    },
+    buttonText: {
+        fontSize: 14,
+        justifyContent: 'flex-start',
+        color: "#000000",
+    },
+    selectedText: {
+        color: "#FBDBE9",
+    },
+});
+
+export default MapTags;
