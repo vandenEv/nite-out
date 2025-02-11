9fc67e631dec2bf8b6bb8007bdc8368aa4513ff9
Leo MacNally <macnalll@tcd.ie>
2025-02-11 12:53:11 +0000

    API calls for publican

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from Gamer import Gamer
 from Publican import Publican
-from Game import Game
+from game import Game
 
 import firebase_admin
 from firebase_admin import credentials, firestore
@@ -22,7 +22,16 @@ def get_gamer():
 
 @app.route("/publican", methods=["GET"])
 def get_publican():
-    pass
+    publican_ref = db_firestore.collection('publicans')
+    docs = publican_ref.stream()
+    publicans = []
+    for doc in docs:
+        publican_data = doc.to_dict()
+        publican_data['id'] = doc.id
+        publicans.append(publican_data)
+    
+    return jsonify(publicans), 200
+
 
 @app.route("/game", methods=["GET"])
 def get_game():
@@ -45,7 +54,19 @@ def create_gamer():
 
 @app.route("/create_publican", methods=["POST"])
 def create_publican():
-    pass
+    data = request.get_json()
+    pub_name = data.get('pub_name')
+    email = data.get('email')
+    ID = data.get('ID')
+    password = data.get('password')
+    address = data.get('address')
+    tables = data.get('tables')
+
+    publican = Publican(pub_name, email, ID, password, address, tables)
+
+    new_publican_ref = db_firestore.collection('publicans').add(publican.pub_details())
+
+    return jsonify({"message": "Publican created", "pub_id": new_publican_ref.id}), 201
 
 @app.route("/create_game", methods=["POST"])
 def create_game():
@@ -71,9 +92,13 @@ def create_game():
 def update_gamer():
     pass
 
-@app.route("/update_publican/<int:publican_id>", methods=["PATCH"])
-def update_publican():
-    pass
+@app.route("/update_publican/<string:publican_id>", methods=["PATCH"])
+def update_publican(publican_id):
+    data = request.get_json()
+    publican_ref = db_firestore.collection('publicans').document(publican_id)
+
+    publican_ref.update(data)
+    return jsonify({"message": "Publican updated successfully!"}), 200
 
 @app.route("/update_game/<string:game_id>", methods=["PATCH"])
 def update_game(game_id):
@@ -85,6 +110,11 @@ def update_game(game_id):
 
     return jsonify({"message": "Game updated successfully!"}), 200
 
+@app.route("/delete_publican/<string:publican_id>", methods=["DELETE"])
+def delete_publican(publican_id):
+    publican_ref = db_firestore.collection('publicans').document(publican_id)
+    publican_ref.delete()
+    return jsonify({"message": "Publican deleted successfully!"}), 200
 
 ## Running this file as main
 if __name__ == "__main__":
