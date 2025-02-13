c73393a840cc598f8613d04477eabdfd69a1bf8c
AskewCow <adambandyk06@gmail.com>
2025-02-13 22:14:47 +0000

    Added Profile Icons

--- frontend/mobile/App.js
+++ frontend/mobile/App.js
@@ -13,68 +13,79 @@ import MapScreen from "./src/screens/MapScreen";
 import MainScreen from "./src/screens/MainScreen";
 import PracticeHomeScreen from "./src/screens/PracticeHomeScreen";
 import ProfileScreen from "./src/screens/ProfileScreen";
+import pfpChoiceScreen from "./src/screens/pfpChoiceScreen";
 
 import { logoXml } from "./src/utils/logo";
 
 const Stack = createStackNavigator();
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator
-        initialRouteName="Login"
-        screenOptions={{
-          headerTitle: () => <SvgXml xml={logoXml} width={40} height={40} />,
-          headerStyle: {
-            elevation: 0,
-            shadowOpacity: 0,
-            backgroundColor: "#00B4D8",
-          },
-          headerLeft: () => null, // Removes Back Button Globally, change if needed
-        }}
-      >
-        <Stack.Screen name="PracticeHome" component={PracticeHomeScreen} />
-        <Stack.Screen
-          name="Main"
-          component={MainScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Map"
-          component={MapScreen}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="SignUp"
-          component={SignUpScreen}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name="PublicanSignUp"
-          component={PublicanSignUpScreen}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name="Login"
-          component={LoginScreen}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name="PassReset"
-          component={PassResetScreen}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name="ResetVerification"
-          component={ResetVerificationScreen}
-          options={{ headerShown: true }}
-        />
-        <Stack.Screen
-          name="Profile"
-          component={ProfileScreen}
-          options={{ headerShown: true }}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                    headerTitle: () => (
+                        <SvgXml xml={logoXml} width={40} height={40} />
+                    ),
+                    headerStyle: {
+                        elevation: 0,
+                        shadowOpacity: 0,
+                        backgroundColor: "#00B4D8",
+                    },
+                    headerLeft: () => null, // Removes Back Button Globally, change if needed
+                }}
+            >
+                <Stack.Screen
+                    name="PracticeHome"
+                    component={PracticeHomeScreen}
+                />
+                <Stack.Screen
+                    name="Main"
+                    component={MainScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="Map"
+                    component={MapScreen}
+                    options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                    name="SignUp"
+                    component={SignUpScreen}
+                    options={{ headerShown: true }}
+                />
+                <Stack.Screen
+                    name="PublicanSignUp"
+                    component={PublicanSignUpScreen}
+                    options={{ headerShown: true }}
+                />
+                <Stack.Screen
+                    name="Login"
+                    component={LoginScreen}
+                    options={{ headerShown: true }}
+                />
+                <Stack.Screen
+                    name="PassReset"
+                    component={PassResetScreen}
+                    options={{ headerShown: true }}
+                />
+                <Stack.Screen
+                    name="ResetVerification"
+                    component={ResetVerificationScreen}
+                    options={{ headerShown: true }}
+                />
+                <Stack.Screen
+                    name="Profile"
+                    component={ProfileScreen}
+                    options={{ headerShown: true }}
+                />
+                <Stack.Screen
+                    name="pfpChoice"
+                    component={pfpChoiceScreen}
+                    options={{ headerShown: true }}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
