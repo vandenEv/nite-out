f66bd2772cfc4482cd34a1993ee61584d2effe2d
Neil Lawrence <lawrencn@tcd.ie>
2025-02-20 11:03:56 +0000

    pub image

--- backend/Publican.py
+++ backend/Publican.py
@@ -1,5 +1,7 @@
+import base64
+
 class Publican:
-    def __init__(self, pub_name, email, ID, password, address, xcoord, ycoord, tables):
+    def __init__(self, pub_name, email, ID, password, address, xcoord, ycoord, tables, pub_image=None):
         self.pub_name = pub_name
         self.email = email
         self.ID = ID
@@ -9,6 +11,7 @@ class Publican:
         self.ycoord = ycoord
         self.tables = tables
         self.events = []
+        self.pub_image = pub_image
 
     def create_event(self, game_type, start_time, end_time, expires, num_units, unit_capacity=None, available_slots=None):
         if game_type not in ["Seat Based", "Table Based"]:
@@ -39,6 +42,19 @@ class Publican:
         self.tables += num_tables
         return {"status": "success", "message": "Reservation cancelled."}
 
+    def update_pub_image(self, new_image):
+        """ can be a file path, Base64 string, or URL) """
+        self.pub_image = new_image
+        return {"status": "success", "message": "Pub image updated."}
+
+    def encode_image(self, image_path):
+        try:
+            with open(image_path, "rb") as img_file:
+                return base64.b64encode(img_file.read()).decode("utf-8")
+        except FileNotFoundError:
+            return {"status": "error", "message": "Image file not found."}
+
+
     def pub_details(self):
         return {
             "pub_name": self.pub_name,
@@ -50,4 +66,5 @@ class Publican:
             "ycoord": self.ycoord,
             "tables": self.tables,
             "events": self.events
+            "pub_image": self.pub_image 
         }
