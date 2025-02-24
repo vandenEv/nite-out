4865b106e0f6f3a431f1289ecb006035019e58dc
ishaanJ91 <jaini@tcd.ie>
2025-02-24 17:27:37 +0000

    added initial setup files

--- backend/tests/test_game.py
+++ backend/tests/test_game.py
@@ -2,8 +2,6 @@ import pytest
 
 import sys
 import os
-
-# Add backend directory to sys.path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from game import Game, SeatBasedGame, TableBasedGame
