c47b3ead169b17caf96622ff0510693c6b45c031
ishaanJ91 <jaini@tcd.ie>
2025-03-16 12:05:31 +0000

    added game_desc attribute

--- backend/tests/fail_test_game.py
+++ backend/tests/fail_test_game.py
@@ -1,6 +1,6 @@
 import pytest
 import sys
-import os
+import os 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from game import Game, SeatBasedGame, TableBasedGame
